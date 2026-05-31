fd23dfd4f544bae45998fd2cc8b712233840e235
김연우 <kyw20304@gmail.com>
2026-05-31 20:20:33 +0900

    fix: 사이드바 지도 섹션 제거

--- src/components/places/PlaceDetail.tsx
+++ src/components/places/PlaceDetail.tsx
@@ -190,17 +190,6 @@ export default function PlaceDetail({ place }: PlaceDetailProps) {
               </a>
             </div>
           </section>
-
-          {/* 작은 지도 (좌표 기준) */}
-          <section className="overflow-hidden rounded-2xl border border-gray-200 bg-white">
-            <div className="h-56 w-full">
-              <KakaoMap
-                center={{ lat: place.lat, lng: place.lng }}
-                places={[place]}
-                selectedPlaceId={place.providerPlaceId}
-              />
-            </div>
-          </section>
         </aside>
       </div>
     </div>
